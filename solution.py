6691485a5d5401ee8b9e4067562e3ad160ad151e
naitax <73228371+naitax@users.noreply.github.com>
2021-09-30 16:02:13 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,8 @@
-'''
+''' Testing #1
 test = {
     'viewonly': [1, 2, 3, 4, 5, 6, 7],
     'admin': [4, 5, 6, 111, 23, 45]
 }
-
 s = input()
 numbers = set(map(int, s.split()))
 l = len(test)
@@ -20,11 +19,8 @@ for i in range(l):
     keys.append(k)
     dicts[keys[i]] = x[i]
     #new_dicts[new_set_roles[i]] = dicts[i]
-
 print(new_set_roles)
-
 #print(dicts)
-
 # print maximum value
 #maximum = max(dicts, key=dicts.get)  # Just use 'min' instead of 'max' for minimum.
 #print(maximum, dicts[maximum])
@@ -35,12 +31,12 @@ for r in dicts_sorted_keys:
 #print(test.values())
 '''
 #################################################################
-test = {
-    'viewonly': ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
-    'admin': ['e', 'f', 'h', 'j', 'i', 'z']
-}
+import json
 
-s = input()
+with open('data.json') as json_file:
+    test = json.load(json_file)
+
+s = input('Please enter names of programs you need to have access to: ').upper()
 numbers = set(map(str, s.split()))
 l = len(test)
 x = []
@@ -55,18 +51,11 @@ for i in range(l):
     k = new_set_roles[i]
     keys.append(k)
     dicts[keys[i]] = x[i]
-    #new_dicts[new_set_roles[i]] = dicts[i]
 
-#print(new_set_roles)
 
-#print(dicts)
-
-# print maximum value
-#maximum = max(dicts, key=dicts.get)  # Just use 'min' instead of 'max' for minimum.
-#print(maximum, dicts[maximum])
-#print(dicts)
 dicts_sorted_keys = sorted(dicts, key=dicts.get, reverse=True)
 for r in dicts_sorted_keys:
-    print('Role: {}, number of programs: {}, list of programs: {}'.format(r.upper(), len(dicts[r]), dicts[r]))
-#print(test.values())
+    if len(dicts[r]) == 0:
+        continue
+    print('Role: {}, number of programs: {}/{}, list of corresponding programs: {}, total list of programs: {}'.format(r.upper(), len(dicts[r]), len(numbers), dicts[r], len(test[r])))
 
